2529a6fe1685c4672b2cdc74e6a1b994c2713a84
Pratham Kubetkar <kubetkarpratham14@gmail.com>
2025-01-12 14:18:22 +0530

    abbbbcd

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='home'),
+    path('', views.index, name='home'),
+    path("data/",views.data,name="data"),
+    path("invoice/",views.getInvoice,name="getInvoice"),
+    # path("print/",views.printInvoice,name="printInvoice"),
 ]
